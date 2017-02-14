adace90520c300304d2a257cfb82664523fce8f2
game <game@iZ2zeerthpe64c45eu5v7eZ.(none)>
2017-02-14 17:28:47 +0800

    edit

--- Item/Item_Equipment.cpp
+++ Item/Item_Equipment.cpp
@@ -11,11 +11,11 @@ Item_Equipment::Item_Equipment()
 {
 }
 
-Item_Equipment::Item_Equipment(Asset::Item_Equipment* stuff)
-{
-	_stuff->set_stuff(stuff->SerializeAsString());
-	_stuff->set_type_t(stuff->type_t());
-}
+//Item_Equipment::Item_Equipment(Asset::Item_Equipment* stuff)
+//{
+//	_stuff->set_stuff(stuff->SerializeAsString());
+//	_stuff->set_type_t(stuff->type_t());
+//}
 
 bool Item_Equipment::CanUse()
 {
--- Item/Item_Equipment.h
+++ Item/Item_Equipment.h
@@ -14,7 +14,7 @@ public:
 	Item_Equipment();
 	~Item_Equipment();
 	
-	explicit Item_Equipment(Asset::Item_Equipment* stuff);
+	//explicit Item_Equipment(Asset::Item_Equipment* stuff);
 	
 	virtual bool CanUse() override;     
 	virtual int32_t Use() override; 
