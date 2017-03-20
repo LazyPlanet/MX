365553943449c6ee6e71bb5ba4eed28c61d97fad
LazyPlanet <1211618464@qq.com>
2017-03-20 18:24:08 +0800

    活动

--- Activity.h
+++ Activity.h
@@ -27,30 +27,13 @@ public:
 		return _instance;
 	}
 	
-	/*
-	bool Load()
-	{
-		std::unordered_set<pb::Message*> messages = AssetInstance.GetMessagesByType(Asset::ASSET_TYPE_ACTIVITY);
-
-		for (auto message : messages)
-		{
-			const Asset::Activity* activity = dynamic_cast<const Asset::Activity*>(message); 
-			if (!activity) return false; //有一个出错即退出
-
-			_list.emplace(activity->common_prop().global_id(), message);
-		}
-
-		return true;
-	}
-	*/
-		
 	bool IsOpen(int64_t global_id)
 	{
 		auto message = AssetInstance.Get(global_id);
-		if (!message) return true; //如果没有配置，则认为是开
+		if (!message) return false; //如果没有配置，则认为是关
 
 		const Asset::Activity* activity = dynamic_cast<const Asset::Activity*>(message);
-		if (!activity) return true;
+		if (!activity) return false;
 
 		boost::posix_time::ptime start_time(boost::posix_time::time_from_string(activity->start_time()));
 		auto start_time_t = boost::posix_time::to_time_t(start_time);
@@ -59,9 +42,9 @@ public:
 		auto stop_time_t = boost::posix_time::to_time_t(stop_time);
 		
 		std::time_t cur_t = CommonTimerInstance.GetTime();
-		if (cur_t >= start_time_t && cur_t <= stop_time_t) return true;
+		if (cur_t < start_time_t || cur_t > stop_time_t) return true;
 
-		return false;
+		return true;
 	}
 };
 
