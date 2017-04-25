15290b0f008f8ca0d8f8c57c7c9d51ca417cfd8a
LazyPlanet <1211618464@qq.com>
2017-04-25 23:51:26 +0800

    玩家花名

---
+++ PlayerName.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+#include <fstream>
+
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+#include "P_Header.h"
+#include "CommonUtil.h"
+
+namespace Adoter
+{
+namespace pb = google::protobuf;
+
+class PlayerName : public std::enable_shared_from_this<PlayerName>   
+{
+	Asset::FamilyName _family_name;
+	Asset::WomanName _woman_name;
+	Asset::ManName _man_name;
+public:
+	static PlayerName& Instance()
+	{
+		static PlayerName _instance;
+		return _instance;
+	}
+
+	void Load()
+	{
+		std::ifstream fi("FamilyName", std::ifstream::in);
+		pb::io::IstreamInputStream fa_iis(&fi);
+		pb::TextFormat::Parse(&fa_iis, &_family_name);
+		fi.close();
+		
+		std::ifstream wi("WomanName", std::ifstream::in);
+		pb::io::IstreamInputStream woman_iis(&wi);
+		pb::TextFormat::Parse(&fa_iis, &_woman_name);
+		wi.close();
+		
+		std::ifstream mi("ManName", std::ifstream::in);
+		pb::io::IstreamInputStream man_iis(&mi);
+		pb::TextFormat::Parse(&fa_iis, &_man_name);
+		mi.close();
+	}
+
+	std::string Get()
+	{
+		auto family_index = CommonUtil::Random(0, _family_name.name().size() - 1);
+		auto woman_index = CommonUtil::Random(0, _woman_name.name().size() - 1);
+		auto man_index = CommonUtil::Random(0, _man_name.name().size() - 1);
+
+		std::string name = woman_index > man_index ? _woman_name.name(woman_index) : _man_name.name(man_index);
+
+		return _family_name.name(family_index) + name; 
+	}
+};
+
+#define NameInstance PlayerName::Instance()
+
+}
